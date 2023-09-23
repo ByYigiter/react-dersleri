f59a6696b3b3d335e11d3aec8c4c17b7eeeabfd1
Yigit Er <mr.yigiter222@gmail.com>
2023-09-23 15:25:01 +0300

    React Dersleri

---
+++ src/components/01-hello-world/HelloWorld.jsx
@@ -0,0 +1,17 @@
+import Comp from "../02-comp/Comp";
+
+
+const HelloWorld = () => {
+
+   
+
+    return (
+        <h1>Hello New Big World 
+            <Comp/>
+        </h1>
+        
+        
+    );
+};
+
+export default HelloWorld;
