f37d3e9aea52ee7d8ab4c176c84b9972408512dd
Yigit Er <mr.yigiter222@gmail.com>
2023-09-30 01:42:49 +0300

    v1

--- src/components/13-products/Footer.jsx
+++ src/components/13-products/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
 	return (
 		<div className="text-center p-5">
-			&copy; 2023 - Copyright by Techno shop
+			&copy; 2023 - Copyright by Yigiter
 		</div>
 	);
 };
