1540316110e8d9d13a31907b53961424127006c1
Yigit Er <mr.yigiter222@gmail.com>
2023-10-27 08:13:37 +0300

    v1

--- src/components/14-state/Stateless.jsx
+++ src/components/14-state/Stateless.jsx
@@ -10,35 +10,34 @@ re-rendering:
  3-Parent component re-render oldugunda
 */
 const Stateless = () => {
-  let mode = "light";
-  let classNames = "bg-light text-dark";
+	let mode = "light";
+	let classNames = "bg-light text-dark";
 
-  const setmode = (m) => {
-    mode = m;
-    
-  if (mode === "dark") {
-    classNames = "bg-dark text-light";
-  }else{
-    classNames = "bg-light text-dark";
-  }
+	const setmode = (m) => {
+		mode = m;
 
-  document.getElementById("switcher").setAttribute("class",classNames);
-  //Bu yontem performansli degildir
-  //Bu kismi yazmayinca da render olayindan dolayi degisikliklerden component haberdar olmuyor
+		if (mode === "dark") {
+			classNames = "bg-dark text-light";
+		} else {
+			classNames = "bg-light text-dark";
+		}
 
-  // return olayı re-render olması için state -prop-ve parent component değişiklik olması gerektr unutma
-  };
-  return (
-    <div id="switcher" className={classNames}>
-      <h1 className="text-center">Dark/Light Mode Switcher</h1>
-      <div className="d-flex justify-content-evenly mt-5 pb-5">
+		document.getElementById("switcher").setAttribute("class", classNames);
+		//Bu yontem performansli degildir
+		//Bu kismi yazmayinca da render olayindan dolayi degisikliklerden component haberdar olmuyor
 
-        {/* ()=> kullanmamaısın amacı hemen otomatik calışmasın basınca calışssın mantığı için  */}
-        <Button onClick={() => setmode("dark")}>Dark Mode</Button>
-        <Button onClick={() => setmode("light")}>Light Mode</Button>
-      </div>
-    </div>
-  );
+		// return olayı re-render olması için state -prop-ve parent component değişiklik olması gerektr unutma
+	};
+	return (
+		<div id="switcher" className={classNames}>
+			<h1 className="text-center">Dark/Light Mode Switcher</h1>
+			<div className="d-flex justify-content-evenly mt-5 pb-5">
+				{/* ()=> kullanmamaısın amacı hemen otomatik calışmasın basınca calışssın mantığı için  */}
+				<Button onClick={() => setmode("dark")}>Dark Mode</Button>
+				<Button onClick={() => setmode("light")}>Light Mode</Button>
+			</div>
+		</div>
+	);
 };
 
 export default Stateless;
