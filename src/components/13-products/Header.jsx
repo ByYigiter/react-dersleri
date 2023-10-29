473bd949aecba1a1ad3ace43f1394a33887f81cd
Yigit Er <mr.yigiter222@gmail.com>
2023-10-29 08:10:14 +0300

    v1

--- src/components/13-products/Header.jsx
+++ src/components/13-products/Header.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import {Container, Nav, Navbar} from "react-bootstrap"
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const Header = () => {
-  return (
-    <Navbar bg="light" expand="lg">
-    <Container>
-      <Navbar.Brand href="#home">Techno Shop</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mx-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Products</Nav.Link>
-          <Nav.Link href="#link">About Us</Nav.Link>
-          <Nav.Link href="#link">Contact Us</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-  )
-}
+	return (
+		<Navbar bg="light" expand="lg">
+			<Container>
+				<Navbar.Brand href="#home">Techno Shop</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="mx-auto">
+						<Nav.Link href="#home">Home</Nav.Link>
+						<Nav.Link href="#link">Products</Nav.Link>
+						<Nav.Link href="#link">About Us</Nav.Link>
+						<Nav.Link href="#link">Contact Us</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+};
 export default Header;
