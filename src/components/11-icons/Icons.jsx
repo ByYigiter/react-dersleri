f4351cc04f3c921d0b011c85cc1fd10019c3fdf9
Yigit Er <mr.yigiter222@gmail.com>
2023-10-31 21:02:19 +0300

    v1

--- src/components/11-icons/Icons.jsx
+++ src/components/11-icons/Icons.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import React from "react";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 import { GiAbstract056 } from "react-icons/gi";
 
-
-
 const Icons = () => {
-  return (
-    <div>
-        react-icon 
-        <AiFillStar  style={{color:"limegreen"}}/>
-        <AiOutlineStar style={{color:"limegreen", fontSize:"5rem"}}/>
-        <GiAbstract056/>
-    </div>
-  )
-}
+	return (
+		<div>
+			react-icon
+			<AiFillStar style={{ color: "limegreen" }} />
+			<AiOutlineStar style={{ color: "limegreen", fontSize: "5rem" }} />
+			<GiAbstract056 />
+		</div>
+	);
+};
 
-export default Icons
+export default Icons;
