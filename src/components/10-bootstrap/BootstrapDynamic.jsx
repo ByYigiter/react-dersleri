2a2d4cc2f7f462b948f968cce102a0a1db2e4455
Yigit Er <mr.yigiter222@gmail.com>
2023-11-01 09:13:12 +0300

    v1

--- src/components/10-bootstrap/BootstrapDynamic.jsx
+++ src/components/10-bootstrap/BootstrapDynamic.jsx
@@ -6,13 +6,12 @@ const BootstrapDynamic = () => {
 	return (
 		<div>
 			<Row>
-            <Col xs={6} md={4}>
+				<Col xs={6} md={4}>
 					xs=6 md=4
 				</Col>
 				<Col xs={12} md={8}>
 					xs=12 md=8
 				</Col>
-				
 			</Row>
 
 			<Carousel data-bs-theme="dark">
