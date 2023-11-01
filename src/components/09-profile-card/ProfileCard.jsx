0385701f7e5df2f45aa2098c09e94c13f217f434
Yigit Er <mr.yigiter222@gmail.com>
2023-11-02 00:27:28 +0300

    v1

--- src/components/09-profile-card/ProfileCard.jsx
+++ src/components/09-profile-card/ProfileCard.jsx
@@ -1,52 +1,41 @@
-import React from 'react'
+import React from "react";
 import "./profile-card.scss";
 
 const ProfileCard = (props) => {
+	const { name, location, shot, followers, following, avatar } = props;
 
-    const { name, location, shot, followers, following, avatar} = props;
+	const img = require(`../../assets/img/${avatar}`);
+	const profileBg = { backgroundImage: `url(${img})` };
 
-    const img = require (`../../assets/img/${avatar}`);
-    const profileBg = {backgroundImage: `url(${img})`}; 
+	return (
+		<div className="profile-card">
+			<div className="profile-header" style={profileBg}></div>
 
-  return (
-    <div className='profile-card'>
+			<div className="profile-content">
+				<div className="profile-avatar" style={profileBg}></div>
 
-        <div className='profile-header' style={profileBg}>
-            
-        </div>
+				<h3>{name}</h3>
+				<h5>{location}</h5>
 
-        <div className='profile-content'>
+				<div className="profile-stats">
+					<div>
+						<h2>{shot}</h2>
+						<span>Shot</span>
+					</div>
 
-            <div className='profile-avatar' style={profileBg}>
-                
-            </div>
+					<div>
+						<h2>{followers}</h2>
+						<span>Followers</span>
+					</div>
 
-            <h3>{name}</h3>
-            <h5>{location}</h5>
+					<div>
+						<h2>{following}</h2>
+						<span>Following</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-            <div className='profile-stats'>
-                <div>
-                    <h2>{shot}</h2>
-                    <span>Shot</span>
-                </div>
-
-                <div>
-                    <h2>{followers}</h2>
-                    <span>Followers</span>
-                </div>
-
-                <div>
-                    <h2>{following}</h2>
-                    <span>Following</span>
-                </div>
-            </div>
-
-        </div>
-
-    </div>
-
-
-  )
-}
-
-export default ProfileCard
+export default ProfileCard;
