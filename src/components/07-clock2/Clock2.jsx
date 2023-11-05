b4fd2e7b78e40444dd1c97c7b22585fde0756dbe
Yigit Er <mr.yigiter222@gmail.com>
2023-11-05 22:36:53 +0300

    v1

--- src/components/07-clock2/Clock2.jsx
+++ src/components/07-clock2/Clock2.jsx
@@ -1,45 +1,43 @@
-import React from 'react';
+import React from "react";
 import "./clock2.scss";
 import moment from "moment";
 
-
 const Clock2 = (props) => {
+	const dateTime = moment();
+	const dateStr = dateTime.format("LL");
+	const timeStr = dateTime.format("HH:mm");
+	const dayStr = dateTime.format("dddd");
 
-    const dateTime = moment();
-    const dateStr = dateTime.format('LL');
-    const timeStr = dateTime.format("HH:mm");
-    const dayStr = dateTime.format("dddd");
+	const hour = dateTime.format("HH");
 
-    const hour = dateTime.format("HH");
+	let message = "";
 
-    let message = "";
+	if (hour >= 6 && hour < 11) {
+		message = "Morning";
+	} else if (hour >= 11 && hour < 17) {
+		message = "Afternoon";
+	} else if (hour >= 17 && hour < 22) {
+		message = "Evening";
+	} else {
+		message = "Night";
+	}
 
-    if(hour>=6 && hour<11) {
-        message="Morning";
-    } else if(hour>=11 && hour<17) {
-        message = "Afternoon";
-    } else if (hour>=17 && hour<22) {
-        message = "Evening";
-    } else {
-        message = "Night";
-    }
+	const clockStyle = {
+		color: props.textColor,
+		backgroundColor: props.bgColor,
+	};
 
+	return (
+		<div className="clock-container" style={clockStyle}>
+			<div className="time">{timeStr}</div>
+			<div>
+				<div className="date">{dateStr}</div>
+				<div className="day">
+					{dayStr} {message}
+				</div>
+			</div>
+		</div>
+	);
+};
 
-    const clockStyle = {
-        color: props.textColor,
-        backgroundColor: props.bgColor
-    }
-
-
-  return (
-    <div className='clock-container' style={clockStyle}>
-        <div className='time'>{timeStr}</div>
-        <div>
-            <div className='date'>{dateStr}</div>
-            <div className='day'>{dayStr} {message}</div>
-        </div>
-    </div>
-  )
-}
-
-export default Clock2
+export default Clock2;
